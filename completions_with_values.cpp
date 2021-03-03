3a73f05d06488dd9df4105bf474ca5de25934827
Saku Myllymäki <myllymakisaku@gmail.com>
2021-03-03 20:58:23 +0200

    random numbers and cartesian product for c++ implementation

--- completions_with_values.cpp
+++ completions_with_values.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cassert>
 #include <algorithm>
+#include <random>
+#include <limits>
 
 using namespace std;
 const double EPS = 1E-9;
@@ -20,29 +22,33 @@ T product(vector<T> &vec) {
     return ret;
 }
 
-int main() {
-    vector<int> dims{ 3, 3, 3 };
-    int ndims = dims.size();
-    assert(ndims > 0);
-    for (auto d : dims) {
-        assert(d > 0);
+template <typename T>
+void cart_product_rec(
+    vector<vector<T>>& in,
+    vector<vector<T>>& out,
+    vector<T> current,
+    int depth
+    ) {
+    if (depth == in.size()) {
+        out.push_back(current);
+        return;
     }
-    assert(ndims == 1 || count(dims.begin(), dims.end(), 1) == 0);
-    int nentries = product(dims);
-    int nvars = sum(dims);
-    int k = 0;
-    vector<vector<int>> paramIndices(ndims);
-    for (int i = 0; i < ndims; i++) {
-        for (int j = 0; j < dims[i]; j++) {
-            paramIndices[i].push_back(k);
-            k++;
-        }
+    int n = in[depth].size();
+    for (int i = 0; i < n; i++) {
+        current.push_back(in[depth][i]);
+        cart_product_rec(in, out, current, depth + 1);
+        current.pop_back();
     }
-    
-    cout << "Hello world " << nentries << " " << nvars << endl;
-    return 0;
 }
 
+template <typename T>
+void cart_product(
+    vector<vector<T>>& in,
+    vector<vector<T>>& out
+    ) {
+    vector<T> current;
+    cart_product_rec(in, out, current, 0);
+}
 
 int compute_rank(vector<vector<double>> A) {
     int n = A.size();
@@ -72,3 +78,43 @@ int compute_rank(vector<vector<double>> A) {
     }
     return rank;
 }
+
+int main() {
+    vector<int> dims{ 3, 3, 3 };
+    int ndims = dims.size();
+    assert(ndims > 0);
+    for (auto d : dims) {
+        assert(d > 0);
+    }
+    assert(ndims == 1 || count(dims.begin(), dims.end(), 1) == 0);
+    int nentries = product(dims);
+    int nvars = sum(dims);
+    int k = 0;
+    vector<vector<int>> paramIndices(ndims);
+    for (int i = 0; i < ndims; i++) {
+        for (int j = 0; j < dims[i]; j++) {
+            paramIndices[i].push_back(k);
+            k++;
+        }
+    }
+    vector<vector<int>> entryparamIndices;
+    cart_product(paramIndices, entryparamIndices);
+
+    vector<double> x(nentries);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> r(numeric_limits<double>::min(), numeric_limits<double>::max());
+    for (int i = 0; i < nentries; i++) x[i] = r(gen);
+    
+    vector<double> tensorentries(nentries);
+    for (int i = 0; i < entryparamIndices.size(); i++) {
+        vector<int> &seq = entryparamIndices[i];
+        vector<double> params(seq.size());
+        for (int j = 0; j < seq.size(); j++) params[j] = x[seq[j]];
+        tensorentries[i] = product(params);
+    }
+
+    cout << "Hello world " << nentries << " " << nvars << endl;
+    return 0;
+}
+
